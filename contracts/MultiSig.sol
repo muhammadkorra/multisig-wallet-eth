aebe9fdd51eac5ef254149f061e464468dbf2e6e
Mohammad Korra <muhammadkorra@gmail.com>
2023-11-18 22:03:23 +0400

    add proposeTransaction and approveTransactionn with corresponding tests

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -2,6 +2,25 @@
 pragma solidity ^0.8.19;
 
 contract MultiSig {
+
+    // define a Transaction structure
+    struct Transaction {
+        // the destination address of the transaction
+        address destination;
+
+        // value of the transaction (in wei)
+        uint256 value;
+
+        // the transaction data
+        bytes data;
+
+        // whether the tansaction has been executed or not (to prevent re-execution)
+        bool executed;
+
+        // quorum received on this transaction (must be > requiredQuorum) for transaction to execute
+        uint256 quorum;
+    }
+
     // list of authorized signers
     address[] public authorizedSigners;
 
@@ -11,34 +30,89 @@ contract MultiSig {
     // wallet owner
     address public owner;
 
+    // to permit O(1) lookup for the signers, instead O(n) every time a function is called
+    mapping(address => bool) public isSigner;
+
+    // the list of transactions since contract deployment
+    Transaction[] public transactions;
+
     // event emitted when a new transaction is proposed
     event TransactionProposed(uint256 trxnId, address proposedBy);
 
-    // event emitted when a signer confirms a transaction
-    event TransactionConfirmed(int256 trxnId, address confirmedBy);
+    // event emitted when a transaction reaches the no. of approvals required for execution
+    event TransactionConfirmed(uint256 trxnId, uint256 quorum);
 
-    // event emitted when the no. of approvals reaches the required quorum
-    event TransactionApproved(uint256 trxnId, uint256 quorum);
+    // event emitted when a signer approves a transaction
+    event TransactionApproved(uint256 trxnId, address approvedBy);
 
     // event emitted when a transaction is executed
     event TransactionExecuted(uint256 trxnId, address destination, address executedBy, uint256 value, bool success);
 
-    // error when failing to confirm transaction
-    error FailedToConfirmTransaction(string reason);
-
     // error when failing to execute a transaction
     error FailedToExecuteTransaction(string reason);
+
+    // allows only authorized signers and owner to perform the action
+    modifier onlySigners() {
+        require(msg.sender == owner || isSigner[msg.sender] , "Only contract owner or authorized signers can perform this action");
+        _;
+    }
 
 
     constructor(address[] memory _signers, uint256 _requiredQuorum) {
         owner = msg.sender;
-        authorizedSigners = _signers;
+        // authorizedSigners = _signers;
+
+        // perform this step to decrease lookup time for signers
+        for (uint i = 0; i < _signers.length; i++) {
+            address signer = _signers[i];
+
+            require(!isSigner[signer], "signer already exists");
+
+            isSigner[signer] = true;
+            authorizedSigners.push(signer);
+        }
+
         requiredQuorum = _requiredQuorum;
     }
     
-    function proposeTransaction(address destination, uint256 _value, bytes memory _data) external {}
+    // this is not protected since "anyone" can propose a transaction
+    function proposeTransaction(address _destination, uint256 _value, bytes memory _data) external{
+        uint256 trxnId = transactions.length;
 
-    function approveTransaction(uint256 _trxnId) external {}
+        // create a new transaction and push it to the transactions list
+        transactions.push(Transaction({
+            destination: _destination,
+            value: _value,
+            data: _data,
+            executed: false,
+            quorum: 0
+        }));
+
+        // emit the proposed event
+        emit TransactionProposed(trxnId, msg.sender);
+    }
+
+    // only contract owner or authorized signers can perform this action
+    function approveTransaction(uint256 _trxnId) external onlySigners {
+        
+        // make sure transaction exists
+        require(transactions.length > _trxnId, "Transaction does not exist");
+
+        // make sure that it is not already executed
+        require(!transactions[_trxnId].executed, "Transaction already executed");
+
+        Transaction storage trxn = transactions[_trxnId];
+
+        // increment number of received confirmations
+        trxn.quorum += 1;
+
+        // emit the confirmed event
+        emit TransactionApproved(_trxnId, msg.sender);
+
+        // if transaction quorum is reached emit the approved event
+        if(trxn.quorum >= requiredQuorum)
+            emit TransactionConfirmed(_trxnId, trxn.quorum);
+    }
 
     function executeTransaction(uint256 _trxnId) external {}
 }
--- test/MultiSig.js
+++ test/MultiSig.js
@@ -39,5 +39,56 @@ describe("MultiSig Contract", function() {
             // verify the second signer
             expect(await multiSig.authorizedSigners(1)).to.equal(signer2.address);
         })
+    });
+
+    describe("Transactions", async function() {
+        it("Should propose a new valid transaction", async function() {
+            const { multiSig } = await loadFixture(deployContractFixture);
+            const signers = await ethers.getSigners();
+            
+            // validate that the transaction emits the right event
+            await expect(multiSig.connect(signers[3]).proposeTransaction(signers[4].address, 100, "0x"))
+                .to.emit(multiSig, "TransactionProposed")
+                .withArgs(0, signers[3].address)
+
+            // get the transaction
+            const transaction = await multiSig.transactions(0);
+            
+            // validate transaction parameters
+           expect(transaction.destination).to.equal(signers[4].address);
+           expect(transaction.value).to.equal(100);
+           expect(transaction.data).to.equal("0x");
+           expect(transaction.executed).to.equal(false);
+           expect(transaction.quorum).to.equal(0);
+        })
+
+        it("Should approve a valid transactions", async function() {
+            const { multiSig, signer1 } = await loadFixture(deployContractFixture);
+            const signers = await ethers.getSigners();
+
+            // propse a transaction from a non-signer
+            await multiSig.connect(signers[3]).proposeTransaction(signers[4].address, 100, "0x");
+
+            // approve the transaction from a valid signer
+            await expect(multiSig.connect(signer1).approveTransaction(0))
+                .to.emit(multiSig, "TransactionApproved")
+                .withArgs(0, signer1.address);
+            
+            const trxn = await multiSig.transactions(0);
+
+            // validate the new quorum count
+            expect(trxn.quorum).to.be.equal(1);
+        })
+
+        it("Should fail to confirm if sender is not a valid signer", async function(){
+            const { multiSig } = await loadFixture(deployContractFixture);
+            const signers = await ethers.getSigners();
+
+            // propose a transaction
+            await multiSig.connect(signers[3]).proposeTransaction(signers[4].address, 100, "0x");
+
+            await expect(multiSig.connect(signers[5]).approveTransaction(0))
+                .to.be.revertedWith("Only contract owner or authorized signers can perform this action");
+        })
     })
 })
