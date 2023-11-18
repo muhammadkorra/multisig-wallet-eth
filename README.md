d48963bb06a426dcd2f72414270180d8e38f1f34
Mohammad Korra <muhammadkorra@gmail.com>
2023-11-18 16:07:25 +0400

    add basic contract structure and update the readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # multisig-wallet-eth
 Multi Signature Wallet Smart Contract in Solidity using Hardhat for EVM-Compatible blockchains.
+
+### Install NPM Dependencies
+```shell
+npm i
+```
+
+### Run The Tests
+
+```shell
+npx hardhat test
+```
+
+### Initialize the Hardhat EVM node
+
+```shell
+npx hardhat node
+```
+
+### Run the "deploy.js" Script
+
+```bash
+npx hardhat --network localhost ./scripts/deploy.js
+```
--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -1,2 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+contract MultiSig {
+    // list of authorized signers
+    address[] public authorizedSigners;
+
+    // required quorum
+    uint256 public requiredQuorum;
+
+    // wallet owner
+    address public owner;
+
+    // event emitted when a new transaction is proposed
+    event TransactionProposed(uint256 trxnId, address proposedBy);
+
+    // event emitted when a signer confirms a transaction
+    event TransactionConfirmed(int256 trxnId, address confirmedBy);
+
+    // event emitted when the no. of approvals reaches the required quorum
+    event TransactionApproved(uint256 trxnId, uint256 quorum);
+
+    // event emitted when a transaction is executed
+    event TransactionExecuted(uint256 trxnId, address destination, address executedBy, uint256 value, bool success);
+
+    // error when failing to confirm transaction
+    error FailedToConfirmTransaction(string reason);
+
+    // error when failing to execute a transaction
+    error FailedToExecuteTransaction(string reason);
+
+
+    constructor(address[] memory _signers, uint256 _requiredQuorum) {
+        owner = msg.sender;
+        authorizedSigners = _signers;
+        requiredQuorum = _requiredQuorum;
+    }
+    
+    function proposeTransaction(address destination, uint256 _value, bytes memory _data) external {}
+
+    function approveTransaction(uint256 _trxnId) external {}
+
+    function executeTransaction(uint256 _trxnId) external {}
+}
